587dad134dfbffd79456f6a56933d4f13ffb550a
1st-Juchara <136792261+1st-Juchara@users.noreply.github.com>
2023-09-11 22:04:03 +0300

    Structure Pipe added

---
+++ Lab-1/Lab-1/Lab-1.cpp
@@ -0,0 +1,16 @@
+﻿// Lab-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+void Menu() {
+    int number;
+    std::cout << "1. Add pipe \n2. Add CS \n3. Objects \n4. Edit pipe \n5. Edit CS \n6. Save \n7. Exit \n> ";
+    std::cin >> number;
+}
+
+int main()
+{
+    Menu();
+    return 0;
+}
