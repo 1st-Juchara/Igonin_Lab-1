d5fe3ae505c4314173a57f8dba9ea9c82d3e2f92
1st-Juchara <136792261+1st-Juchara@users.noreply.github.com>
2023-09-30 11:42:19 +0300

    New methods has been added

--- Lab-1/Lab-1/Lab-1.cpp
+++ Lab-1/Lab-1/Lab-1.cpp
@@ -21,6 +21,22 @@ struct CS {
     int payoff = 0;
 };
 
+bool isExist(const Pipe& pipe)
+{
+    return pipe.length > 0 ? true : false;
+}
+
+bool isExist(const CS& Stations)
+{
+    return Stations.WS.size() > 0 ? true : false;
+}
+
+void inputString(istream& in, string& str)
+{
+    in.ignore(10000, '\n');
+    getline(in, str);
+}
+
 float tryInputNum(float min, float max) {
     float num;
     cout << "\n\n> ";
@@ -51,28 +67,36 @@ int Menu() {
     return number;
 }
 
-void View(Pipe pipe, CS Stations) {
-    if (pipe.length > 0){
-        cout << "Pipe" << endl;
-        cout << "\t\t" << "Name: " << pipe.name << endl;
-        cout << "\t\t" << "Length: " << pipe.length << endl;
-        cout << "\t\t" << "Diameter: " << pipe.diameter << endl;
-        if (pipe.inRepare)
-            cout << "\t\t" << "Status: IN REPAIR" << endl;
+void ViewPipe(const Pipe& pipe)
+{
+    cout << "Pipe" << endl;
+    cout << "\t\t" << "Name: " << pipe.name << endl;
+    cout << "\t\t" << "Length: " << pipe.length << endl;
+    cout << "\t\t" << "Diameter: " << pipe.diameter << endl;
+    if (pipe.inRepare)
+        cout << "\t\t" << "Status: IN REPAIR" << endl;
+    else
+        cout << "\t\t" << "Status: EXPLOITED \n" << endl;
+}
+
+void ViewCS(const CS& Stations)
+{
+    cout << "Stations" << endl;
+    cout << "\t\t" << "Name: " << Stations.name << endl;
+    cout << "\t\t" << "Number of workshops: " << Stations.WS.size() << endl;
+    cout << "\t\t" << "Payoff: " << Stations.payoff << endl;
+    for (int j = 0; j < Stations.WS.size(); j++)
+        if (Stations.WS[j])
+            cout << "\t\t WS " << j + 1 << ". ON" << endl;
         else
-            cout << "\t\t" << "Status: EXPLOITED \n" << endl;
-    }
-    if (Stations.WS.size() > 0) {
-        cout << "Stations" << endl;
-        cout << "\t\t" << "Name: " << Stations.name << endl;
-        cout << "\t\t" << "Number of workshops: " << Stations.WS.size() << endl;
-        cout << "\t\t" << "Payoff: " << Stations.payoff << endl;
-        for (int j = 0; j < Stations.WS.size(); j++)
-            if (Stations.WS[j])
-                cout << "\t\t WS " << j + 1 << ". ON" << endl;
-            else
-                cout << "\t\t WS " << j + 1 << ". OFF" << endl;
-    }
+            cout << "\t\t WS " << j + 1 << ". OFF" << endl;
+}
+
+void View(Pipe& const pipe, CS& const Stations) {
+    if (isExist(pipe))
+            ViewPipe(pipe);
+    if (isExist(Stations))
+        ViewCS(Stations);
     cout << endl;
 }
 
@@ -80,8 +104,7 @@ void addPipe(Pipe& pipe) {
     int t = 0;
     cout << "Enter pipe name:\n\n> ";
     string name;
-    cin.ignore(10000, '\n');
-    getline(cin, name);
+    inputString(cin, name);
     cout << endl;
     cout << "Enter pipe length:";
     float length = tryInputNum(0, INT_MAX);
@@ -95,30 +118,16 @@ void addCS(CS& Stations) {
     int wsOn = 0;
     std::cout << "Enter CS name:\n\n> ";
     string name;
-    cin.ignore(10000, '\n');
-    getline(cin, name);
+    inputString(cin, name);
     cout << endl;
     std::cout << "Enter count of workshops: ";
     int workshops = tryChoose(1, 10000);
     vector <bool> wsStatus;
-    for (int i = 0; i < workshops; i++) {
-        cout << i+1 << ". Workshop status: \n\t1. ON\n\t2. OFF";
-        int status = tryChoose(1, 2);
-        switch (status)
-        {
-        case 1:
-            wsStatus.push_back(true);
-            wsOn++;
-            break;
-        case 2:
-            wsStatus.push_back(false);
-            break;
-        default:
-            cout << "WRONG KEY";
-            break;
-        }
-    }
-    Stations = { name, wsStatus, wsOn / workshops * 100 };
+    wsStatus.resize(workshops);
+    fill(wsStatus.begin(), wsStatus.end(), true);
+    std::cout << "Enter station productivity (1..100): ";
+    int prod = tryChoose(1, 100);
+    Stations = { name, wsStatus, prod };
 }
 
 void editPipe(Pipe& pipe) {
@@ -126,17 +135,11 @@ void editPipe(Pipe& pipe) {
         cout << pipe.name << endl;
         cout << "\tPipe status:" << endl;
         cout << "\t\t1. UNDER REPAIR \n\t\t2. IS FUNCTIONING";
-        int status = tryChoose(1, 2);
-        if (status == 1)
-            pipe.inRepare = true;
-        else
-            pipe.inRepare = false;
+        pipe.inRepare = tryChoose(1, 2) == 1;
     }
 }
 
-void editCS(CS& Stations) { // Добавь возможность редактировать сразу несколько WS за раз
-    if (Stations.WS.size() > 0)
-    {
+void editCS(CS& Stations) {
         cout << Stations.name << endl;
         cout << "\tPayoff: " << Stations.payoff << endl;
         cout << "\tWorkshops: " << Stations.payoff << endl;
@@ -146,94 +149,108 @@ void editCS(CS& Stations) { // Добавь возможность редакт�
             else
                 cout << "\t\t" << i + 1 << ". WS is OFF" << endl;
         cout << endl;
+        cout << "";
         cout << "Number of WS for editing:";
         int WScount = tryChoose(1, Stations.WS.size());
         for (int i = 0; i < WScount; i++)
         {
             cout << "Enter index of WS for editing:";
             int WSindex = tryChoose(1, Stations.WS.size());
-            Stations.WS[WSindex-1] = !Stations.WS[WSindex-1];
+            Stations.WS[WSindex - 1] = !Stations.WS[WSindex - 1];
         }
-    }
 }
 
-void Save(Pipe& pipe, CS& Stations) 
+void PipeDataOut(ofstream& fout, const Pipe& pipe)
+{
+    fout << 1;
+    fout << pipe.name << endl;
+    fout << pipe.length << ' ' << pipe.diameter << ' ' << pipe.inRepare << endl;
+}
+
+void CSDataOut(ofstream& fout, const CS& Stations)
+{
+    fout << 1;
+    fout << Stations.name << endl;
+    fout << Stations.payoff;
+    fout << ' ' << Stations.WS.size();
+    for (int i = 0; i < Stations.WS.size(); i++)
+    {
+        fout << ' ' << Stations.WS[i];
+    }
+    fout << endl;
+}
+
+void Save(const Pipe& pipe, const CS& Stations) 
 {
     ofstream fout("Saves.txt", ios_base::out | ios_base::trunc);// out - открыте для записи, trunc - удаление содержимого, ios_base - класс для всех потоковых классов ввода-вывода
     if (fout.is_open())
     {
-        if ((pipe.length > 0) || (Stations.WS.size() > 0))
+        if (isExist(pipe) || isExist(Stations))
         {
-            if (pipe.length > 0)
+            if (isExist(pipe))
             {
-                fout << 1;
-                fout << pipe.name << endl;
-                fout << pipe.length << ' ' << pipe.diameter << ' ' << pipe.inRepare << endl;
+                PipeDataOut(fout, pipe);
             }
             else
                 fout << 0 << endl;
-            if (Stations.WS.size() > 0)
+            if (isExist(Stations))
             {
-                if (Stations.WS.size() > 0)
-                {
-                    fout << 1;
-                    fout << Stations.name << endl;
-                    fout << Stations.payoff;
-                    fout << ' ' << Stations.WS.size();
-                    for (int i = 0; i < Stations.WS.size(); i++)
-                    {
-                        fout << ' ' << Stations.WS[i];
-                    }
-                    fout << endl;
-                }
-                else
-                    fout << 0 << endl;
-
+                CSDataOut(fout, Stations);
             }
             else
                 fout << 0 << endl;
-            fout.close();
             cout << "Save completed" << endl;
         }
         else
             cout << "Nothing to save" << endl;
         cout << endl;
+        fout.close();
     }
     else
         cout << "Error: save was failed";
 }
 
+void PipeDataIn(ifstream& in, Pipe& pipe)
+{
+    int pipeCnt = 0;
+    in >> pipeCnt;
+    if (pipeCnt > 0)
+    {
+        getline(in, pipe.name);
+        in >> pipe.length;
+        in >> pipe.diameter;
+        in >> pipe.inRepare;
+    }
+}
+
+void CSDataIn(ifstream& in, CS& Stations)
+{
+    int StationsCnt = 0;
+    in >> StationsCnt;
+    if (StationsCnt > 0)
+    {
+        getline(in, Stations.name);
+        in >> Stations.payoff;
+        int WScount = 0;
+        in >> WScount;
+        Stations.WS.resize(WScount);
+        for (int i = 0; i < WScount; i++)
+        {
+            bool ws = true;
+            in >> ws;
+            Stations.WS[i] = ws;
+        }
+    }
+}
+
 void Load(Pipe& pipe, CS& Stations)
 {
-    ifstream fout("Saves.txt");
-    if (fout.is_open())
+    ifstream fin("Saves.txt");
+    if (fin.is_open())
     {
-        int pipeCnt = 0;
-        fout >> pipeCnt;
-        if (pipeCnt > 0)
-        {
-            getline(fout, pipe.name);
-            fout >> pipe.length;
-            fout >> pipe.diameter;
-            fout >> pipe.inRepare;
-        }
-        int StationsCnt = 0;
-        fout >> StationsCnt;
-        if (StationsCnt > 0)
-        {
-            getline(fout, Stations.name);
-            fout >> Stations.payoff;
-            int WScount = 0;
-            fout >> WScount;
-            Stations.WS.resize(WScount);
-            for (int i = 0; i < WScount; i++)
-            {
-                bool ws = true;
-                fout >> ws;
-                Stations.WS[i] = ws;
-            }
-        }
-        fout.close();
+        PipeDataIn(fin, pipe);
+        CSDataIn(fin, Stations);
+        fin.close();
         cout << "Load completed" << endl;
     }
     else
@@ -243,8 +260,7 @@ void Load(Pipe& pipe, CS& Stations)
 
 int main()
 {
-    Pipe pipe;
-    pipe.length = 0;
+    Pipe pipe = {};
     CS Stations;
     while (true) {
         int number = Menu();
@@ -278,5 +294,5 @@ int main()
             break;
         }
     };
-    return 1;
+    return 0;
 }
